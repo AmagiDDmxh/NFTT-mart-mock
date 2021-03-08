73a320036ae2a169d02f520cd2da5e6ad725c284
amagi <amagiddmxh@qq.com>
2021-03-08 17:55:57 +0800

    trying to fix serving problem the fifth time

--- ormconfig.js
+++ ormconfig.js
@@ -14,19 +14,35 @@ module.exports = [
       subscribersDir: "src/subscriber"
     }
   },
+  // {
+  //   name: "production",
+  //   type: "postgres",
+  //   url: process.env.DATABASE_URL,
+  //   synchronize: true, // switch this to false once you have the initial tables created and use migrations instead
+  //   logging: false,
+  //   entities: ["dist/entity/**/*.js"],
+  //   migrations: ["dist/migration/**/*.js"],
+  //   subscribers: ["dist/subscriber/**/*.js"],
+  //   cli: {
+  //     entitiesDir: "dist/entity",
+  //     migrationsDir: "dist/migration",
+  //     subscribersDir: "dist/subscriber"
+  //   }
+  // },
   {
     name: "production",
     type: "postgres",
     url: process.env.DATABASE_URL,
     synchronize: true, // switch this to false once you have the initial tables created and use migrations instead
-    logging: false,
-    entities: ["dist/entity/**/*.js"],
-    migrations: ["dist/migration/**/*.js"],
-    subscribers: ["dist/subscriber/**/*.js"],
+    logging: true,
+    entities: ["src/entity/**/*.js"],
+    migrations: ["src/migration/**/*.js"],
+    subscribers: ["src/subscriber/**/*.js"],
     cli: {
-      entitiesDir: "dist/entity",
-      migrationsDir: "dist/migration",
-      subscribersDir: "dist/subscriber"
+      entitiesDir: "src/entity",
+      migrationsDir: "src/migration",
+      subscribersDir: "src/subscriber"
     }
   }
+
 ];
