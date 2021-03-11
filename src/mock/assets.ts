ef7d767a4caf74b470d62c7e12b2da6be02b80c8
amagi <amagiddmxh@qq.com>
2021-03-11 22:46:40 +0800

    feat: add cursor and enhancec mock data

--- src/mock/assets.ts
+++ src/mock/assets.ts
@@ -1,7 +1,7 @@
 import casual from "casual";
 
 const status = [1, 2, 3];
-const collectionIds = [1, 2, 3];
+const collectionIds = casual.array_of_integers(88);
 const categoryIds = [1, 2, 3, 4, 5];
 
 type Asset = {
@@ -19,12 +19,15 @@ type Asset = {
   collectionId: number;
   createdAt: string;
   updatedAt: string;
-}
+};
 
-const DEFAULT_DATA: Asset[] = [
-  {
-    id: 1,
-    name: casual.name,
+const AMOUNT = 888;
+
+const DEFAULT_DATA: Asset[] = Array(AMOUNT)
+  .fill(0)
+  .map((_, index) => ({
+    id: index + 1,
+    name: casual.state,
     picUrl:
       "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
     status: casual.random_element(status),
@@ -38,231 +41,6 @@ const DEFAULT_DATA: Asset[] = [
     updatedAt: "2021-02-26T00:59:52+08:00",
     price: casual.double(1, 100000),
     latestPrice: casual.double(1, 100000),
-  },
-  {
-    id: 2,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 3,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 4,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 5,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 6,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 7,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 8,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 9,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 10,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 11,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 12,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 13,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 14,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-  {
-    id: 15,
-    name: casual.name,
-    picUrl:
-      "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80",
-    status: casual.random_element(status),
-    address: "0x12541254189999",
-    describe: casual.sentence,
-    metadata: casual.sentence,
-    externalLinks: casual.sentence,
-    collectionId: casual.random_element(collectionIds),
-    categoryId: casual.random_element(categoryIds),
-    createdAt: "2021-02-26T00:59:52+08:00",
-    updatedAt: "2021-02-26T00:59:52+08:00",
-    price: casual.double(1, 100000),
-  },
-]
+  }));
 
 export default DEFAULT_DATA;
--- src/resolvers/AssetResolver.ts
+++ src/resolvers/AssetResolver.ts
@@ -48,11 +48,9 @@ export class AssetResolver {
     @Arg("page", () => Int, { nullable: true }) page = 1,
     @Arg("status", () => Int, { nullable: true }) status = -1,
     @Arg("categoryId", () => Int, { nullable: true }) categoryId = -1,
-    @Arg("collectionId", () => Int, { nullable: true }) collectionId = -1
+    @Arg("collectionId", () => Int, { nullable: true }) collectionId = -1,
+    @Arg("id", () => Int, { nullable: true }) id = -1
   ): Promise<PaginatedAssets> {
-    const realLimit = Math.min(20, pageSize);
-    const currentPageNumber = (page - 1) * pageSize;
-
     const filteredAssets = ASSETS.filter(
       ({ status: originalStatus }) => status === -1 || status === originalStatus
     )
@@ -62,17 +60,21 @@ export class AssetResolver {
       )
       .filter(
         ({ collectionId: cId }) => collectionId === -1 || collectionId === cId
-      );
+      )
+      .filter(({ id: originalId }) => id === -1 || originalId === id);
+
+    const startCursor = (page - 1) * pageSize;
+    const endCursor = Math.min(filteredAssets.length, page * pageSize);
 
     return {
-      assets: (filteredAssets as any).slice(currentPageNumber, realLimit),
-      hasMore: false,
+      assets: (filteredAssets as any).slice(startCursor, endCursor),
+      hasMore: !!filteredAssets.length && endCursor < filteredAssets.length,
     };
   }
 
   @Query(() => PaginatedCollections)
   async collections(
-    @Arg("pageSize", () => Int, { nullable: true }) pageSize = 10,
+    @Arg("pageSize", () => Int, { nullable: true }) pageSize = 100,
     @Arg("page", () => Int, { nullable: true }) page = 1,
     @Arg("id", () => Int, { nullable: true }) id = -1
   ): Promise<PaginatedCollections> {
@@ -92,13 +94,12 @@ export class AssetResolver {
     };
   }
 
-
   @Query(() => PaginatedCollections)
   async myCollections(
     @Arg("pageSize", () => Int, { nullable: true }) pageSize = 10,
     @Arg("page", () => Int, { nullable: true }) page = 1,
     @Arg("id", () => Int, { nullable: true }) id = -1,
-    @Arg("user", () => Int, { nullable: true }) user = '',
+    @Arg("user", () => Int, { nullable: true }) user = ""
   ): Promise<PaginatedCollections> {
     const realLimit = Math.min(20, pageSize);
     const currentPageNumber = (page - 1) * pageSize;
